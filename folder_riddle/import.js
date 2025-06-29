0eac3283291ad6121225218db3be8b2a40a091b5
shlomi noyshtat <s0583244814@gmail.com>
2025-06-29 15:33:46 +0300

    adding import riddles

--- folder_riddle/import.js
+++ folder_riddle/import.js
@@ -1,6 +1,16 @@
 import rr1 from './r1.js'
 import rr2 from './r2.js'
 import rr3 from './r3.js'
+import rr4 from './r4.js'
+import rr5 from './r5.js'
+import rr6 from './r6.js'
+import rr7 from './r7.js'
+import rr8 from './r8.js'
+import rr9 from './r9.js'
+import rr10 from './r10.js'
+import rr11 from './r11.js'
+import rr12 from './r12.js'
 
-export default  [rr1,rr2,rr3]
+
+export default  [rr1,rr2,rr3,rr4,rr5,rr6,rr7,rr8,rr9,rr10,rr11,rr12]
 
